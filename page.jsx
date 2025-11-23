600acdf243bdb70d8a260278c6941783f4e842e0
oscarbravo044-source <oscarbravo044@gmail.com>
2025-11-23 01:03:02 -0500

    Add files via upload

---
+++ page.jsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Checkout Page</h1>
+      <p>Your clean project is ready. Add your custom UI here.</p>
+    </main>
+  );
+}
